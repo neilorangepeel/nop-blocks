1d74746f1c3e312b3f7ea763f5769cc5693e8196
Neil Hainsworth <howdy@neilorangepeel.com>
2022-04-18 18:41:46 +0000

    Adding github.php

---
+++ github.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The webhook from github to pull changes from development to live.
+ *
+ * @see https://code.tutsplus.com/tutorials/the-perfect-workflow-with-git-github-and-ssh--net-19564 by Jeffery Way
+ *
+ * @package nop-blocks
+ */
+	`git pull`;
+?>
